75f5d785d67ee2b94e93637df8a2470b4c07555a
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-17 23:59:05 +0530

    Security Auto-Patch: Fix GHSA-8cf7-32gw-wr33 in jsonwebtoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken@9.0.3');
 
+function authenticateToken(req, res, next) {
+  const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
+  const token = req.header('Authorization').replace('Bearer ', '');
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Invalid token');
+    }
+    req.user = decoded;
+    next();
+  });
+}
 const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
 
 /**
