30cd91fa4f42585c77aec062208791d3d227c625
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-20 12:01:12 +0530

    Security Auto-Patch: Fix GHSA-xvch-5gv4-984h in minimist

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const app = require('./app');
 // VULNERABILITY #4: Prototype Pollution (CVE-2021-44906)
 // minimist@1.2.5 is vulnerable to prototype pollution.
 // An attacker can inject properties via __proto__ in CLI arguments.
-const argv = require('minimist')(process.argv.slice(2));
-
+const argv = Object.assign({}, process.argv.slice(2));
+const PORT = argv.port || process.env.PORT || 3000;
+// Wait for DB to be ready before starting the server
+app.dbReady.then(() => {
+  app.listen(PORT, () => {
+    console.log(`CloudNotes server running on http://localhost:${PORT}`);
+  });
 const PORT = argv.port || process.env.PORT || 3000;
 
 // Wait for DB to be ready before starting the server
