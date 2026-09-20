37a42599f77442ee29ff1218194e2b8578b96696
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-20 12:00:20 +0530

    Security Auto-Patch: Fix GHSA-qwph-4952-7xr6 in jsonwebtoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,13 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require('jsonwebtoken@9.0.3'); // update to the fixed version
+function authenticateToken(req, res, next) {
+  const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
+  const token = req.header('Authorization').replace('Bearer ', '');
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) return next(err);
+    req.user = decoded;
+    next();
+  });
+}
 const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
 
 /**
