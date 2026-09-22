02d1c245f23392f1117e9dcbfc6d7ed945b48e4c
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-22 21:39:09 +0530

    Security Auto-Patch: Fix CVE-2026-44487 in axios

--- routes/attachments.js
+++ routes/attachments.js
@@ -6,8 +6,7 @@ const { authenticateToken } = require('../middleware/auth');
 // VULNERABILITY #7: Server-Side Request Forgery (CVE-2021-3749)
 // axios@0.21.1 does not properly handle user-controlled URLs,
 // allowing SSRF attacks to internal services.
-const axios = require('axios');
-
+const axios = require('axios@1.16.0'); // or require('axios@0.32.0') depending on the version you want to use
 // VULNERABILITY #6 (cont.): ReDoS via moment date parsing
 const moment = require('moment');
 
