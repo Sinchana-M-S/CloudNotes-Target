a4d7be271b606ee6b2a106e7604729b103495395
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-22 21:44:28 +0530

    Security Auto-Patch: Fix CVE-2022-21681 in marked

--- routes/render.js
+++ routes/render.js
@@ -5,8 +5,19 @@ const { authenticateToken } = require('../middleware/auth');
 // VULNERABILITY #8: ReDoS in Markdown Parsing (CVE-2022-21681)
 // marked@0.3.9 is vulnerable to Regular Expression Denial of Service.
 // Specially crafted markdown input can cause catastrophic backtracking.
-const marked = require('marked');
+const marked = require('marked@4.0.10'); // Update marked package to the fixed version
+const router = express.Router();
 
+router.get('/:id', authenticateToken, (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const markdown = req.query.markdown;
+    const html = marked.parse(markdown);
+    res.send(html);
+  } catch (err) {
+    res.status(500).send('Error rendering note content');
+  }
+})
 const router = express.Router();
 
 // GET /api/render/:id - Render a note's content as HTML
