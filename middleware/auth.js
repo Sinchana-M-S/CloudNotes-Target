3ed682ae811a544aa7c9963d26fa0e1e5917d491
Sinchana-M-S <sinchanams932@gmail.com>
2026-09-26 10:11:08 +0530

    Security Auto-Patch: Fix CVE-2022-23539 in jsonwebtoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,13 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require('jsonwebtoken@9.0.0'); // Update the package version to the fixed version
+function authenticateToken(req, res, next) {
+  const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).send({ message: 'Invalid token' });
+    req.user = decoded; // Assuming the token contains the user data
+    next();
+  });
+}
 const JWT_SECRET = 'cloudnotes-super-secret-key-2024';
 
 /**
